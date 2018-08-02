2999a4609a010539f50b64c0b13d5121d528ba72
speedmancs <speedmancs@gmail.com>
2018-08-01 22:07:11 -0700

    266,293,370,750

---
+++ source/medium/DP/750_NumberOfCornerRactangles.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int countCornerRectangles(vector<vector<int>>& grid) {
+        if (grid.size() == 0 || grid[0].size() == 0) return 0;
+        int ROW = grid.size();
+        int COL = grid[0].size();
+        int count = 0;
+        vector<vector<int>> dp(COL, vector<int>(COL, 0));
+        for (int r = 0; r < ROW; r ++)
+        {
+            for (int c1 = 0; c1 < COL - 1; c1++)
+            {
+                if (grid[r][c1] == 0) continue;
+                for (int c2 = c1 + 1; c2 < COL; c2 ++)
+                {
+                    if (grid[r][c2] == 0) continue;
+                    count += dp[c1][c2];
+                    dp[c1][c2] ++;
+                }
+            }
+        }
+        return count;
+    }
+};
