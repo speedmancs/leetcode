a90c97e85a3a5098e8ea27cc7e36f2e84a4b998d
speedmancs <speedmancs@gmail.com>
2018-08-02 21:15:31 -0700

    209

---
+++ source/medium/Array/209_Minumum_SizeSubArraySum_On.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int left = 0;
+        int right = 0;
+        // a[left: right], right is fixed, move forward left
+        // as left is the first index in range[0: right] so that sum(a[left: right]) < s
+        int ans = INT_MAX;
+        int sum = nums[0];
+        while (right < n)
+        {
+            while (left <= right && sum >= s)
+            {
+                ans = min(ans, right - left + 1);
+                sum -= nums[left];
+                left ++;
+            }            
+            right ++;
+            if (right < n) sum += nums[right];
+        }
+
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
